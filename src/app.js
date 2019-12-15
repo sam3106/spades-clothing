a6a18d4483c7d876b6d50220354de66c6ca4b566
sam3106 <sampson2448@yahoo.com>
2019-12-15 10:29:17 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./app.css";
+import HomePage from "./homepage";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
