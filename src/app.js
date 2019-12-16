f33b6328549fabdf794f99940ddb1c94daf58650
sam3106 <sampson2448@yahoo.com>
2019-12-16 10:42:32 -0500

    update nav withRouter

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./app.css";
-import HomePage from "./homepage";
+import HomePage from "./pages/HomePage/homepage";
+import { Switch, Route } from "react-router-dom";
 
+const HatsPage = () => (
+  <div>
+    <h1> Hi</h1>
+  </div>
+);
 class App extends React.Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route  path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/component/menu-item/menu-item.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl})` }}
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem);
